20d6112ff98414b16ca278aa67b62176b034b018
mjeffw <geek.macbeer@gmail.com>
2024-05-13 08:32:22 -0400

    initial commit

---
+++ gb-quick-reference/scripts/gb-quickref-sheet.js
@@ -0,0 +1,126 @@
+const parselink = GURPS.parselink
+const handlePdf = GURPS.handlePdf
+
+export default class GBQuickReferenceSheet extends GURPS.ActorSheets.character {
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ['gb-quickref-sheet', 'sheet', 'actor'],
+      width: 640,
+      height: 800,
+      dragDrop: [{ dragSelector: '.item-list .item', dropSelector: null }],
+    })
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  get template() {
+    return `/modules/saethors-bane-dfrpg/gb-quick-reference/templates/gb-quickref-sheet.hbs`
+  }
+
+  getData() {
+    const data = super.getData()
+
+    data.pageref = getProperty(this.actor, 'flags.gurps.pageref')
+    data.copyright = getProperty(this.actor, 'flags.gurps.copyright') ?? '©2024 Gaming Ballistic, LLC'
+    data.torso = this.actor.getTorsoDr()
+    data.parryblock = this.actor.getEquippedParry()
+
+    switch (getProperty(this.actor, 'flags.gurps.book')) {
+      case 'NBB':
+        data.cssClass = `${data.cssClass} bugstiary`
+        break
+
+      case 'NBG':
+        data.cssClass = `${data.cssClass} garden`
+        break
+
+      case 'NBS':
+        data.cssClass = `${data.cssClass} snakes`
+        break
+
+      case 'NB':
+        data.cssClass = `${data.cssClass} bestiary`
+        break
+    }
+    return data
+  }
+
+  getCustomHeaderButtons() {
+    return []
+  }
+
+  get gurpsActorData() {
+    return this.actor.getGurpsActorData()
+  }
+
+  /**
+   * @returns {Array<{mode: string, value: number, default: boolean}>}
+   * 
+   * Returns an array of move objects, each with a mode (GROUND, AIR, WATER, SPACE, UNDERGROUND, etc), a value (number
+   * of yards per turn), and an optional default (boolean). If default = true, this is the value used in the 
+   * Encumbrance table that displays adjusted move.
+   * 
+   * It sources its data from either a `system.move` object, if it exists, or directly from `system.basicmove.value`. 
+   * `system.move` should be used when there are multiple movement modes, or when Ground move is not present at all, or 
+   * when actual move per turn is different from basicmove, as when the actor has the Enhanced Move advantage.
+
+   * The modes are also such that we can replace them with 'GURPS.move${mode}' to derive an i18n language key to get
+   * a label for display (e.g., 'GURPS.moveGROUND'). If no such i18n key exists, the label uses the mode without
+   * translation.
+   */
+  // _getMove() {
+  //   return this.gurpsActorData.move ?? [{ mode: 'GROUND', value: gurpsActorData.basicmove.value, default: true }]
+  // }
+
+  activateListeners(html) {
+    super.activateListeners(html)
+
+    document
+      .querySelector('.gb-quick-reference')
+      .style.setProperty('--labelAcc', `"${game.i18n.localize('GURPS.acc')} "`)
+    document
+      .querySelector('.gb-quick-reference')
+      .style.setProperty('--labelBulk', `"${game.i18n.localize('GURPS.bulk')} "`)
+    document.querySelector('.gb-quick-reference').click(ev => {
+      this._onfocus(ev)
+    })
+
+    html.find('.rollableicon').click(this._onClickRollableIcon.bind(this))
+    html.find('.gb-move').click(this._onClickMove.bind(this))
+    html.find('.gb-link').click(this._handleOnPdfLink.bind(this))
+
+    html.find('[data-onethird]').hover(
+      function () {
+        let opt = $(this).attr('data-onethird')
+        let msg = 'Disable&nbsp;' + opt
+        if ($(this).hasClass('buttongrey')) msg = 'Enable&nbsp;' + opt
+        $(this).find('div').last().remove()
+        $(this).append($(`<div class="hover-help">${msg}</div>`))
+      },
+      function () {
+        $(this).find('div').last().remove()
+      }
+    )
+  }
+
+  async _onClickRollableIcon(ev) {
+    ev.preventDefault()
+    const element = ev.currentTarget
+    const val = element.dataset.value
+    const parsed = parselink(val)
+    GURPS.performAction(parsed.action, this.actor, ev)
+  }
+
+  _onClickMove(event) {
+    event.preventDefault()
+    const value = event.currentTarget.dataset.key
+    this.actor.setMoveDefault(value)
+  }
+
+  _handleOnPdfLink(event) {
+    const prefix = getProperty(this.actor, 'flags.gurps.book')
+    handlePdf(prefix + event.currentTarget.innerText)
+  }
+}
---
+++ gb-quick-reference/scripts/init.js
@@ -0,0 +1,239 @@
+export default async function init(module) {
+  Handlebars.registerHelper('gb-version', function () {
+    return version === '%%version%%' ? 'DEV' : version
+  })
+
+  Handlebars.registerHelper('gb-entries', function (map) {
+    return map ? Object.values(map) : []
+  })
+
+  Handlebars.registerHelper('gb-mergeLists', function (a, b) {
+    return [...a, ...b]
+  })
+
+  Handlebars.registerHelper('gb-sort', function (list, options) {
+    let sortfield = options.hash?.sortfield
+    list.sort((a, b) => a[sortfield].localeCompare(b[sortfield]))
+    return list
+  })
+
+  Handlebars.registerHelper('gb-round', function (text) {
+    let value = typeof text === 'number' ? text : parseFloat(text)
+    value = Math.round(value * 100) / 100
+    return value === 0 ? 0 : value
+  })
+
+  // TODO Can be replaed with the "tracker" helper from the GURPS system after it is updated.
+  Handlebars.registerHelper('gb-tracker', function (data, trackerName) {
+    if (!!data && !!data.additionalresources?.tracker) {
+      // find the tracker with name
+      let tracker = Object.values(data.additionalresources?.tracker).find(it => it.name === trackerName)
+      if (tracker) {
+        let found = Object.keys(data.additionalresources.tracker).find(
+          it => data.additionalresources.tracker[it].name === trackerName
+        )
+        tracker.key = found
+        return tracker
+      }
+    }
+    return null
+  })
+
+  const thresholdLabelMap = {
+    Grabbed: '1/10×',
+    Grappled: '1/2×',
+    Restrained: 'CM',
+    Controlled: '1.5×',
+    Pinned: '2×',
+  }
+
+  Handlebars.registerHelper('gb-thresholdAbbrev', function (threshold) {
+    return thresholdLabelMap[threshold] ?? ''
+  })
+
+  const toRemove = ['Natural Attacks']
+  Handlebars.registerHelper('gb-traitFilter', function (traits) {
+    const array = objectToArray(traits)
+    return arrayToObject(
+      array.filter(trait => !toRemove.includes(trait.name)),
+      5
+    )
+  })
+
+  /**
+   * For now, just handle integer addition.
+   *
+   * The expected input is a string that can be parsed into <number>+<number> after removing all spaces.
+   */
+  Handlebars.registerHelper('gb-calculate', function (formula, options) {
+    const ADD_INTS = /^\s*(?<left>(?:[\-\–]?[0-9]+))\s*\+\s*(?<right>(?:[\-\–]?[0-9]+))\s*$/i
+    const matches = formula.toString().match(ADD_INTS)
+    if (matches) {
+      const left = parseInt(matches.groups.left)
+      const right = parseInt(matches.groups.right)
+      return `${left + right}`
+    }
+    return formula
+  })
+
+  Handlebars.registerHelper('gb-collapseMelee', function (array) {
+    let results = []
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == null) continue
+      let melee = array[i]
+      let item = {
+        name: melee.name,
+        level: melee.level,
+        notes: melee.notes,
+        parry: melee.parry,
+        cost: melee.cost,
+        damage: [
+          {
+            damage: melee.damage,
+            reach: melee.reach,
+            followup: melee.followup,
+            damagenotes: melee.damagenotes,
+            mode: melee.mode,
+          },
+        ],
+      }
+
+      for (let j = 0; j < array.length; j++) {
+        if (j !== i && array[j] !== null)
+          if (melee.name === array[j].name && melee.level === array[j].level && melee.notes === array[j].notes) {
+            item.damage.push({
+              damage: array[j].damage,
+              reach: array[j].reach,
+              followup: array[j].followup,
+              damagenotes: array[j].damagenotes,
+              mode: array[j].mode,
+            })
+            array[j] = null
+          }
+      }
+      results.push(item)
+    }
+    return results
+  })
+
+  Handlebars.registerHelper('gb-collapseRanged', function (array) {
+    let results = []
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == null) continue
+      let ranged = array[i]
+
+      // if damage looks like <damage>/<number>point<s> then set damage to <damage> and cost to <number>
+      const regex = /(?<damage>.*?)\/(?<points>\d+)?\s*point(?:s)?/
+      if (ranged.damage.match(regex)) {
+        const groups = ranged.damage.match(regex).groups
+        ranged.damage = `+${groups.damage}`
+        ranged.cost = +(groups.points || 1)
+      }
+
+      let item = {
+        name: ranged.name,
+        level: ranged.level,
+        notes: ranged.notes,
+        acc: ranged.acc,
+        bulk: ranged.bulk,
+        range: ranged.range,
+        shots: ranged.shots,
+        rcl: ranged.rcl,
+        cost: ranged.cost,
+        damagecomponent: [
+          {
+            damage: ranged.damage,
+            followup: ranged.followup,
+            damagenotes: ranged.damagenotes,
+          },
+        ],
+      }
+
+      for (let j = 0; j < array.length; j++) {
+        if (j !== i && array[j] !== null)
+          if (
+            ranged.name === array[j].name &&
+            ranged.level === array[j].level &&
+            ranged.notes === array[j].notes &&
+            ranged.acc === array[j].acc &&
+            ranged.bulk === array[j].bulk &&
+            ranged.range === array[j].range &&
+            ranged.shots === array[j].shots &&
+            ranged.rcl === array[j].rcl &&
+            ranged.cost === array[j].cost
+          ) {
+            item.damagecomponent.push({
+              damage: array[j].damage,
+              followup: array[j].followup,
+              damagenotes: array[j].damagenotes,
+            })
+            array[j] = null
+          }
+      }
+      results.push(item)
+    }
+    return results
+  })
+
+  fetch(`/modules/${module}/gb-quick-reference/templates/attack-ranged.hbs`)
+    .then(it => it.text())
+    .then(async text => {
+      Handlebars.registerPartial('gb-attack-ranged', text)
+    })
+
+  fetch(`/modules/${module}/gb-quick-reference/templates/attack-melee.hbs`)
+    .then(it => it.text())
+    .then(async text => {
+      Handlebars.registerPartial('gb-attack-melee', text)
+    })
+
+  //  globalThis.GURPS = GURPS
+  const GBQuickReferenceSheet = (await import('./gb-quickref-sheet.js')).default
+
+  Actors.registerSheet('gurps', GBQuickReferenceSheet, {
+    types: ['enemy', 'character'],
+    label: 'GB Quick Reference Sheet',
+    makeDefault: true,
+  })
+}
+
+/**
+ * Converts any Array into an Object. Each element of the array will be a property of
+ * the returned object, with a key equal to the index into the array converted to a
+ * String of length <indexLength>. The String is padded with leading zeros to get the
+ * required length.
+ *
+ * @param {Array<any>} array
+ * @param {Number} indexLength - number of characters to use as the property key
+ * @returns Object
+ */
+export function arrayToObject(array, indexLength = 4) {
+  let data = /** @type {{[key: string]: string}} */ ({})
+  array.forEach((item, index) => {
+    data[GURPS.genkey(index, indexLength)] = item
+  })
+  return data
+}
+
+/**
+ * Converts an object into an array of its property values. The order of properties will be the
+ * natural sorting order of the keys.
+ *
+ * WARNING:
+ * The intent is to provide the reverse of the arrayToObject function (above), where an array is
+ * converted into an Object. The property values in that case are the elements of the array, and
+ * the keys are the element's index converted into a zero-padded string: 0 -> "0000", 1 -> "0001",
+ * 12 -> "0012", etc.
+ *
+ * However, there are no sanity checks of any kind done to ensure that the object matches the
+ * output of the arrayToObject() method; any object will be converted to an array of its property
+ * values, losing the values of its keys if they do not correspond to the array index.
+ *
+ * @param {Object|{ [key: string]: any}} object
+ * @returns {any[]} an Array of property values
+ */
+export function objectToArray(object) {
+  return Object.keys(object)
+    .sort((a, b) => a.localeCompare(b))
+    .map(key => object[key])
+}
