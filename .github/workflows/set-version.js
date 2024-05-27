8837af08f77e0f1ea42666795dcfd6529a318651
mjeffw <geek.macbeer@gmail.com>
2024-05-27 14:48:53 -0400

    actions

--- .github/workflows/set-version.js
+++ .github/workflows/set-version.js
@@ -1,15 +1,17 @@
-var fs = require('fs');
-const manifest = JSON.parse(fs.readFileSync('module.json', 'utf8'));
+var fs = require('fs')
+const manifest = JSON.parse(fs.readFileSync('module.json', 'utf8'))
 
 // first argument is node, second is the filename of the script, third is the version we pass in our workflow.
 // expected tag format is 'refs/tags/v{major}.{minor}.{patch}"
-const tagVersion = process.argv[2].split('/').slice(-1)[0];
+const tagVersion = process.argv[2].split('/').slice(-1)[0]
 
 if (!tagVersion || !tagVersion.startsWith('v')) {
-    console.error(`Invalid version specified: ${tagVersion}`);
-    process.exitCode = 1;
+  console.error(`Invalid version specified: ${tagVersion}`)
+  process.exitCode = 1
 } else {
-    manifest.version = tagVersion.substring(1); // strip the 'v'-prefix
-    fs.writeFileSync('module.json', JSON.stringify(manifest, null, 2)); // pretty print JSON back to module.json
-    console.log(tagVersion);
+  manifest.version = tagVersion.substring(1) // strip the 'v'-prefix
+  manifest.manifest = `https://raw.githubusercontent.com/${process.env.GITHUB_REPOSITORY}/${tagVersion}/module.json`
+  manifest.download = `https://github.com/mjeffw/${process.env.GITHUB_REPOSITORY}/releases/download/${tagVersion}/module.zip`
+  fs.writeFileSync('module.json', JSON.stringify(manifest, null, 2)) // pretty print JSON back to module.json
+  console.log(tagVersion)
 }
